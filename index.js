283e76564b037a81325a0329f8b458ccf765a550
parham-snk <psanjanaki@gmail.com>
2025-06-13 14:36:11 +0330

    some debug

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-import {exec} from "child_process"
+import { exec } from "child_process";
+import { clearInterval } from "timers";
 
 
-let s=exec('docker stats')
 
-s.stdout.on("data",data=>{
-    // console.log(data)
-    // console.log(data.toString().split(' '))
-    // let data.tostring().split('').filter()
-    let ContainerName,memoryUsage,cpuUsage;
-    ContainerName=data.toString().split("/")[3].split('   ')[2]
-    cpuUsage=data.toString().split("/")[3].split('   ')[3]
-    memoryUsage=data.toString().split("/")[3].split('   ')[4]
-    let info={
-        ContainerName,memoryUsage,cpuUsage
-    }
-    console.table(info)
+
+let timer=setInterval((x) => {
+    let s = exec('docker stats --no-stream', (err, stdout) => {
+        let str = stdout.toString();
+        // بررسی کن str شامل حداقل 4 خط هست که خط چهارم واقعی باشه
+        let lines = str.split("\n");
+        if (lines.length >= 2) {
+            //\s میشه فضای خالی
+            // {2,} میشه تعداد تکرار مورد قبلش که فضای خالی هست
+            let columns = lines[1].split(/\s{2,}/); // شکستن بر اساس چند فاصله
+            let ContainerName = columns[1];
+            let cpuUsage = columns[2];
+            let memoryUsage = columns[3];
+
+            let info = {
+                ContainerName,
+                cpuUsage,
+                memoryUsage
+            };
+
+            console.log(info);
+        }
+    })
     
-})
+  
+}, 500);
+
+
+
+setTimeout(() => {
+    clearInterval(timer)
+}, 4000);
